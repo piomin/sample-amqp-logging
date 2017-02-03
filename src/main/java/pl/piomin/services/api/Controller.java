e0aa61ff728b1ba311ba85cc06eb00050526ffbc
piotr.minkowski <piotr.minkowski@gmail.com>
2017-02-03 10:17:56 +0100

    Initial version

---
+++ src/main/java/pl/piomin/services/api/Controller.java
@@ -0,0 +1,20 @@
+package pl.piomin.services.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	protected Logger logger = LoggerFactory.getLogger(Controller.class.getName());
+
+	@RequestMapping("/hello/{param}")
+	public String hello(@PathVariable("param") String param) {
+		logger.info("Controller.hello(" + param + ")");
+		return "Hello";
+	}
+
+}
