b7212b614d29a21ea39342e70513eaf119d010b5
piomin <piotr.minkowski@gmail.com>
2023-02-09 22:05:28 +0100

    enable auto-import

---
+++ src/test/java/pl/piomin/services/api/ApplicationRunTest.java
@@ -0,0 +1,14 @@
+package pl.piomin.services.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+public class ApplicationRunTest {
+
+//    @Test
+//    public void start() {
+//
+//    }
+}
