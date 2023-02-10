80a9f59b06c8b5a8d603310800d9cd4ba8293d14
piomin <piotr.minkowski@gmail.com>
2023-02-10 09:56:58 +0100

    update tests + add dynamic property

--- src/test/java/pl/piomin/services/api/ApplicationRunTest.java
+++ src/test/java/pl/piomin/services/api/ApplicationRunTest.java
@@ -2,13 +2,25 @@ package pl.piomin.services.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
 public class ApplicationRunTest {
 
-//    @Test
-//    public void start() {
-//
-//    }
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    public void start() {
+        String res = restTemplate.getForObject("/hello/{param}", String.class, "123");
+        assertNotNull(res);
+        assertEquals("Hello", res);
+    }
 }
